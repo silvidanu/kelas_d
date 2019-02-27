f9e89d7bb3e260fb7a8bdeb716054462c55e122e
ahmad <ahmad@gmail.com>
2019-02-27 22:34:36 +0700

    Tambah Tugas 1

---
+++ TUGAS_1/5520117107_Ahmad_Ginanjar.php
@@ -0,0 +1,26 @@
+<?php
+function kanguru($a1,$b1,$a1,$b2) {
+    $kelinci1 = $a1. " ";
+    $kelinci2 = $a2. " ";
+         
+        echo "kanguru(".$a1.",".$b1.",".$a2.",".$b2.")";
+        echo "<br>";
+        for ($i=0; $i <4; $i++) {
+            $a1 += $b1;
+            $a2 +=$b2;
+            @$kanguru1.= $a1. " ";
+            @$kanguru2.= $a2. " ";
+        }
+    echo "kanguru1 = ".$kanguru1;
+    echo "<br>";
+    echo "kanguru2 = ".$kanguru2;
+    echo "<br>";
+    if($a1 == $a2 ) {
+        echo "<b> yes </b> <br></br>";
+    } else {
+        echo "<b> no </b> <br> </br>";
+    }
+}
+kanguru(1,3,5,1);
+kanguru(4,2,1,5);
+?>
